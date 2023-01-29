eece87caab2b0e6462ea5939cf56431ac641c78c
Irene Atek <irene.atek@btinternet.com>
2023-01-29 19:50:47 +0000

    initial checking:  test code and classes for team personel structure

---
+++ index.js
@@ -0,0 +1,6 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
